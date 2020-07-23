862295139550162da28050e86c112a2fe2a59f0a
Scott <scott.zetterstrom@gmail.com>
2020-07-23 08:22:08 -0600

    Initial Commit

---
+++ src/main/java/com/example/CRUDCheckpoint/CrudCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDCheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDCheckpoint/CrudCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDCheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
